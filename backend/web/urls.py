9702ee2d23096963c75ddc811ab64197b2922461
kwen <kwen.zou@outlook.com>
2026-02-23 21:48:39 +0800

    实现编辑资料前端

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -10,6 +10,7 @@ from web.views.user.account.login import LoginView
 from web.views.user.account.logout import LogoutView
 from web.views.user.account.refresh_token import RefreshTokenView
 from web.views.user.account.register import RegisterView
+from web.views.user.profile.update import UpdateProfileView
 
 urlpatterns = [
     path('api/user/account/login/', LoginView.as_view(), name='login'),
@@ -17,6 +18,7 @@ urlpatterns = [
     path('api/user/account/register/', RegisterView.as_view(), name='register'),
     path('api/user/account/refresh_token/', RefreshTokenView.as_view(), name='refresh_token'),
     path('api/user/account/get_user_info/', GetUserInfoView.as_view(), name='get_user_info'),
+    path('api/user/profile/update/', UpdateProfileView.as_view(), name='update_profile'),
     path('', index),
     re_path(r'^(?!media/|static/|assets/).*$', index)
 ]
