d23ea74e03b2450b13acdbfe7937bc13d192246a
aikwen <kwen.zou@outlook.com>
2026-03-01 14:40:34 +0800

    打补丁

--- frontend/src/views/create/character/components/Photo.vue
+++ frontend/src/views/create/character/components/Photo.vue
@@ -2,7 +2,7 @@
 import Croppie from 'croppie'
 import 'croppie/croppie.css'
 
-import {onBeforeUnmount, ref, useTemplateRef, watch} from "vue";
+import {nextTick, onBeforeUnmount, ref, useTemplateRef, watch} from "vue";
 import CameralIcon from "@/views/profile/components/icons/CameralIcon.vue";
 import modal from "daisyui/components/modal/index.js";
 
@@ -19,6 +19,7 @@ let croppie = null
 
 async function openModal(photo){
   modalRef.value.showModal()
+  await nextTick()
 
   if (!croppie) {
       croppie = new Croppie(croppieRef.value, {  // 创建croppie对象
