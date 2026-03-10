33c0120886e1af238e44037d6577dbc02db884a8
aikwen <kwen.zou@outlook.com>
2026-03-10 21:55:43 +0800

    实现聊天记录的流式加载

---
+++ backend/web/views/friend/message/chat/graph.py
@@ -0,0 +1,43 @@
+import os
+from typing import TypedDict, Sequence
+
+from langchain_core.messages import BaseMessage
+from langchain_openai import ChatOpenAI
+from langgraph.constants import START, END
+from langgraph.graph import add_messages, StateGraph
+from typing_extensions import Annotated
+
+
+
+class ChatGraph:
+    @staticmethod
+    def create_app():
+        llm = ChatOpenAI(
+            model="deepseek-v3.2",
+            api_key=os.getenv("API_KEY"),
+            base_url=os.getenv("API_BASE"),
+            streaming=True,
+            model_kwargs={
+                "stream_options": {
+                    "include_usage": True,  # 输出token消耗数量
+                }
+            }
+        )
+
+        class AgentState(TypedDict):
+            messages: Annotated[Sequence[BaseMessage], add_messages]
+
+        def model_call(state: AgentState):
+            res = llm.invoke(state['messages'])
+            return {'messages': [res]}
+
+        graph = StateGraph(AgentState)
+        graph.add_node('agent', model_call)
+
+        graph.add_edge(START, 'agent')
+
+        graph.add_edge('agent', END)
+
+        return graph.compile()
+
+
